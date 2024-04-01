1b492e90a19b74fa2a4dac7812458502376b53d0
Carlos Conejo <carlos.conejo-barcelo@renault.com>
2024-04-01 18:53:59 +0200

    upgrade to version 0.1.1 that prints output file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ft2bt',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=required,  # Use the list from requirements.txt
     entry_points={
