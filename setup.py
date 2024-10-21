d60aef6f11fb807c72b46a2b06a229811599ca5c
Carlos Conejo <carlos.conejo-barcelo@renault.com>
2024-10-21 10:38:44 +0200

    update version with summary prints

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+# Read the long description from README.md
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='ft2bt',
-    version='0.5.1',
+    version='0.6.1',
     packages=find_packages(),
     install_requires=required,  # Use the list from requirements.txt
     entry_points={
@@ -17,7 +21,7 @@ setup(
     author='Carlos Conejo',
     author_email='carlos.conejo@upc.edu',
     description='Automatic conversion from fault trees into behavior trees with formal verification capabilities',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/cconejob/ft2bt_converter.git',
     license='GNU GPLv3',
@@ -27,4 +31,8 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    package_data={
+        'ft2bt.scripts.formal_verification': ['smv_header.txt'],
+    },
+    include_package_data=True,
 )
